8c67c1a873d1307671e3c54f3c0c814bcdaa44f8
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-12-25 18:20:11 -0300

    refactoring code deleting context by composition patterns (complete)

--- src/App/useTodos.js
+++ src/App/useTodos.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
-const TodoContext = React.createContext();
 
-function TodoProvider(props) {
+function useTodos() {
     const {
         item: todos, 
         saveItem: saveTodos,
@@ -46,9 +45,7 @@ function TodoProvider(props) {
       newTodos.splice(todoIndex, 1);
       saveTodos(newTodos);
     };
-    return (
-<TodoContext.Provider 
-    value={{
+    return {
        loading, 
        error,
        totalTodos, 
@@ -61,13 +58,9 @@ function TodoProvider(props) {
        deleteTodo, 
        openModal,
        setOpenModal,
-}}>
-        {props.children}
-</TodoContext.Provider>
-    );
-
-}
+      };
+    };
 
 
-export {TodoContext, TodoProvider};
+export { useTodos };
 
