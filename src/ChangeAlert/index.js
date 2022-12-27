49d72dfd3339951e22a3c652d3109247949b034b
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-12-27 19:33:36 -0300

    refactoring code 11

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { withStorageListener } from "./withStorageListener";
+import { useStorageListener } from "./useStorageListener";
 import './ChangeAlert.css'; 
 
 
-function ChangeAlert({show, toggleShow}) {
+function ChangeAlert(sincronize) {
+    const { show, toggleShow } = useStorageListener(sincronize);
+
     if (show) {
         return (
             <div className="ChangeAlert-bg">
@@ -23,7 +25,5 @@ function ChangeAlert({show, toggleShow}) {
           return null;
         }
       }
-      
-      const ChangeAlertWithStorageListener = withStorageListener(ChangeAlert);
-      
-      export { ChangeAlertWithStorageListener };
+            
+      export { ChangeAlert };
